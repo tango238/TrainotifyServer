c7acea64caf3b569b79ef43223ab2aa5fe7e49c6
Go Tanaka <tanago3@gmail.com>
2011-10-02 16:20:13 +0900

    デリミタを排除

--- server.js
+++ server.js
@@ -14,14 +14,15 @@ io.sockets.on('connection', function (socket) {
   socket.on('googleRequest',function(data){ 
     var client = http.createClient(80, 'www.google.co.jp');
     var request = client.request('GET', '/m/directions?ttype=last&dirflg=r&hl=ja&dirflg=r&hl=ja&ri=0&saddr='+ data.from  +'&daddr=' + data.to, {});
-    var content = '';
     request.on('response', function(response){
-      response.on('data', function(chunk){
-        content = chunk;
-        socket.emit('idCode', { data: content + ''});
-        socket.emit('idCode', { data: data.delim});
+      response.on('data', function(content){
+        socket.emit('idCode', { data: content + '' });
       }); 
     }); 
     request.end();
+  });
+  socket.on('error',function(data){ 
+    // Do nothing
+    console.log('error');
   });
 });
